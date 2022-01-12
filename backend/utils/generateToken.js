3ba78cfc126dca3c569dc744e13b9610ea42e36f
Francis Inose <kizzycole10@gmail.com>
2022-01-12 23:32:39 +0000

    updated jwt token again

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 import  jwt  from "jsonwebtoken";
 
 const generateToken = (id) => {
-    return jwt.sign({id}, `${process.env.JWT_SECRET}`, {
+    return jwt.sign({ id }, `${process.env.JWT_SECRET}`, {
         expiresIn: '30d'
     })
 }
